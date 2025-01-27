14511667fe2d05a74e3f2df2c548b8607ed281a4
chenxiiZ <12czeng@gmail.com>
2025-01-28 04:08:27 +0800

    add

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,23 @@
-import './index.css'
-import Navbar from "./frontend/components/Navbar.tsx"
-function App() {
+import './index.css';
+import Navbar from './frontend/components/Navbar.tsx';
+import {  Routes, Route } from 'react-router-dom';
+import Classify from './frontend/routes/Classify.tsx';
+import Education from './frontend/routes/Education.tsx';
+import GetInvolved from './frontend/routes/GetInvolved.tsx';
+import ContactUs from './frontend/routes/ContactUs.tsx';
 
-  return <Navbar />
+function App() {
+  return (
+      <>
+          <Navbar /> {/* Render Navbar */}
+          <Routes> {/* Define Routes */}
+              <Route path="/classify" element={<Classify />} />
+              <Route path="/education" element={<Education />} />
+              <Route path="/get-involved" element={<GetInvolved />} />
+              <Route path="/contact-us" element={<ContactUs />} />
+          </Routes>
+      </>
+  );
 }
 
-export default App
+export default App;
